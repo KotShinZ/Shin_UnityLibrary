f6c921afdb39c950496078571a9606eb9689b2ce
KotShinZ <s18574s18574@gmail.com>
2024-04-19 12:40:09 +0900

    update git ignore

---
+++ Core/Shin_UnityLibrary_Data/SceneNameEnum.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// シーン名を管理する Enum
+/// </summary>
+public enum SceneNameEnum
+{
+None,
+	Title,
+	Loading,
+	Maneger,
+	OnlineSelect,
+	LocalSelect,
+	CharactorSelect,
+	OnlineBattle,
+	LocalBattle,
+	Simple,
+	GrassStage,
+	StageBase,
+}
