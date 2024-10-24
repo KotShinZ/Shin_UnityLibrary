9738e2f3ffdba5f1c75a32b44e0da343c5114ee4
KotShinZ <s18574s18574@gmail.com>
2024-10-25 06:45:23 +0900

    create SetRendererSortingLayer

---
+++ Core/Component/SetRendererSortingLayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRendererSortingLayer : MonoBehaviour
+{
+    public SortingLayer sortingLayer;
+    public int sortingOrder = 0;
+
+    void OnValidate()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if(renderer == null)
+        {
+            return;
+        }
+        //renderer.sortingLayerName = sortingLayer.name;
+        renderer.sortingOrder = sortingOrder;
+    }
+}
