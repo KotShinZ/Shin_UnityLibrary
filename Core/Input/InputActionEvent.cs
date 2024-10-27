10126580953ffeba4710cc9ba28273a4238c4448
KotShinZ <s18574s18574@gmail.com>
2024-10-27 15:09:03 +0900

    InputActionEvent

---
+++ Core/Input/InputActionEvent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class InputActionEvent : MonoBehaviour
+{
+    public InputAction action;
+    [Space(10)]
+    public UnityEvent<InputAction.CallbackContext> started;
+    public UnityEvent<InputAction.CallbackContext> performed;
+    public UnityEvent<InputAction.CallbackContext> canceled;
+
+    private void Awake()
+    {
+        action.Enable();
+        action.started += ctx => started.Invoke(ctx);
+        action.performed += ctx => performed.Invoke(ctx);
+        action.canceled += ctx => canceled.Invoke(ctx);
+    }
+}
