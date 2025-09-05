ccf513d47dbab62096d28078bd3075651768055f
KotShinZ <s18574s18574@gmail.com>
2025-09-06 05:21:04 +0900

    this is cause of error.

--- OverideAssets/Photon/PhotonEventInstantiate.cs
+++ OverideAssets/Photon/PhotonEventInstantiate.cs
@@ -119,7 +119,7 @@ public class PhotonEventInstantiate : MonoBehaviour, IOnEventCallback
 
         // 受信したtransformを設定
         
-        var obj = Instantiate(GetNetworkPrefab((string)data[0]), (Vector3)data[1], (Quaternion)data[2]);
+        var obj = GameObject.Instantiate(GetNetworkPrefab((string)data[0]), (Vector3)data[1], (Quaternion)data[2]);
         if(obj.TryGetComponent(out PhotonObjectSynchronizer pos))
         {
             pos.setMine = false;
